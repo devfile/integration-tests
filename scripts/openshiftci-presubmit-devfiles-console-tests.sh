48dc26c37afb71c908ab554545f5798d5cd4fe71
Joseph Kim <joskim@redhat.com>
2021-02-09 15:54:18 -0500

    Update test-cypress.sh

--- scripts/openshiftci-presubmit-devfiles-console-tests.sh
+++ scripts/openshiftci-presubmit-devfiles-console-tests.sh
@@ -6,5 +6,5 @@ set -e
 set -x
 
 git clone https://github.com/openshift/console $GOPATH/src/github.com/openshift/console
-cp scripts/openshiftci-presubmit-devfiles-console-tests.sh $GOPATH/src/github.com/openshift/console/
+cp scripts/console/test-cypress.sh $GOPATH/src/github.com/openshift/console/
 cd $GOPATH/src/github.com/openshift/console

555113548e616533a7ab153d50a789557916ad89
Joseph Kim <joskim@redhat.com>
2021-02-10 13:00:58 -0500

    remove unnecessary tests

---
+++ scripts/console/test-prow-e2e.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+ARTIFACT_DIR=${ARTIFACT_DIR:=/tmp/artifacts}
+SCREENSHOTS_DIR=frontend/gui_test_screenshots
+INSTALLER_DIR=${INSTALLER_DIR:=${ARTIFACT_DIR}/installer}
+
+function copyArtifacts {
+  if [ -d "$ARTIFACT_DIR" ] && [ -d "$SCREENSHOTS_DIR" ]; then
+    echo "Copying artifacts from $(pwd)..."
+    cp -r "$SCREENSHOTS_DIR" "${ARTIFACT_DIR}/gui_test_screenshots"
+  fi
+}
+
+trap copyArtifacts EXIT
+
+# don't log kubeadmin-password
+set +x
+BRIDGE_KUBEADMIN_PASSWORD="$(cat "${KUBEADMIN_PASSWORD_FILE:-${INSTALLER_DIR}/auth/kubeadmin-password}")"
+export BRIDGE_KUBEADMIN_PASSWORD
+set -x
+BRIDGE_BASE_ADDRESS="$(oc get consoles.config.openshift.io cluster -o jsonpath='{.status.consoleURL}')"
+export BRIDGE_BASE_ADDRESS
+
+# Add htpasswd IDP
+oc apply -f ./frontend/integration-tests/data/htpasswd-secret.yaml
+oc patch oauths cluster --patch "$(cat ./frontend/integration-tests/data/patch-htpasswd.yaml)" --type=merge
+
+# "fake" dbus address to prevent errors
+# https://github.com/SeleniumHQ/docker-selenium/issues/87
+DBUS_SESSION_BUS_ADDRESS=/dev/null
+export DBUS_SESSION_BUS_ADDRESS
+
+SCENARIO="${1:-e2e}"
+
+#if [ "$SCENARIO" != "login" ] && [ "$SCENARIO" != "olmFull" ] && [ "$SCENARIO" != "ceph" ]; then
+#  CHROME_VERSION=$(google-chrome --version) ./test-protractor.sh "$SCENARIO"
+#fi
+
+# Disable color codes in Cypress since they do not render well CI test logs.
+# https://docs.cypress.io/guides/guides/continuous-integration.html#Colors
+export NO_COLOR=1
+if [ "$SCENARIO" == "e2e" ] || [ "$SCENARIO" == "release" ]; then
+  ./test-cypress.sh -h true
+#elif [ "$SCENARIO" == "login" ]; then
+#  ./test-cypress.sh -p console -s 'tests/app/auth-multiuser-login.spec.ts' -h true
+#elif [ "$SCENARIO" == "olmFull" ]; then
+#  ./test-cypress.sh -p olm -h true
+#elif [ "$SCENARIO" == "ceph" ]; then
+#  ./test-cypress.sh -p ceph -h true
+elif [ "$SCENARIO" == "kubevirt-gating" ]; then
+  ./test-cypress.sh -p kubevirt -h true
+fi

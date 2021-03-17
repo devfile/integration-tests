6b75f97c314b9c30723395b7dc9d019bc1cbfd41
Joseph Kim <joskim@redhat.com>
2021-03-17 15:57:49 -0400

    remove static package.json

--- scripts/openshiftci-presubmit-devfiles-console-tests.sh
+++ scripts/openshiftci-presubmit-devfiles-console-tests.sh
@@ -10,7 +10,9 @@ git clone https://github.com/openshift/console $GOPATH/src/github.com/openshift/
 cp scripts/console/test-cypress.sh $GOPATH/src/github.com/openshift/console/
 cp scripts/console/test-prow-e2e.sh $GOPATH/src/github.com/openshift/console/
 
-cp scripts/console/frontend/package.json $GOPATH/src/github.com/openshift/console/frontend/
+sed -i.bak 's|"test-cypress-devconsole": "cd packages/dev-console/integration-tests && ../../../node_modules/.bin/cypress open --env openshift=true",|"test-cypress-devconsole-headless-create-from-devfile": "cd packages/dev-console/integration-tests && node --max-old-space-size=4096 ../../../node_modules/.bin/cypress run --env openshift=true --browser ${BRIDGE_E2E_BROWSER_NAME:=chrome} --headless --spec \"features/addFlow/create-from-devfile.feature\";",|' $GOPATH/src/github.com/openshift/console/frontend/package.json
+rm $GOPATH/src/github.com/openshift/console/frontend/package.json.bak
+
 cp scripts/console/frontend/packages/dev-console/integration-tests/features/addFlow/create-from-devfile.feature $GOPATH/src/github.com/openshift/console/frontend/packages/dev-console/integration-tests/features/addFlow/
 
 cd $GOPATH/src/github.com/openshift/console

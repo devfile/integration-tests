7e2e4a94370cc5a99b9663db56904580af3a9abf
Joseph Kim <joskim@redhat.com>
2021-03-19 14:14:27 -0400

    update

--- scripts/openshiftci-presubmit-devfiles-console-tests.sh
+++ scripts/openshiftci-presubmit-devfiles-console-tests.sh
@@ -10,7 +10,8 @@ git clone https://github.com/openshift/console $GOPATH/src/github.com/openshift/
 cp scripts/console/test-cypress.sh $GOPATH/src/github.com/openshift/console/
 cp scripts/console/test-prow-e2e.sh $GOPATH/src/github.com/openshift/console/
 
-sed -i'' '/test-cypress-devconsole-headless/a \\t"test-cypress-devconsole-headless-create-from-devfile": "cd packages/dev-console/integration-tests && node --max-old-space-size=4096 ../../../node_modules/.bin/cypress run --env openshift=true --browser ${BRIDGE_E2E_BROWSER_NAME:=chrome} --headless --spec \\"features/addFlow/create-from-devfile.feature\\";"\,' $GOPATH/src/github.com/openshift/console/frontend/package.json
+#sed -i'' '/test-cypress-devconsole-headless/a \\t"test-cypress-devconsole-headless-create-from-devfile": "cd packages/dev-console/integration-tests && node --max-old-space-size=4096 ../../../node_modules/.bin/cypress run --env openshift=true --browser ${BRIDGE_E2E_BROWSER_NAME:=chrome} --headless --spec \\"features/addFlow/create-from-devfile.feature\\";"\,' $GOPATH/src/github.com/openshift/console/frontend/package.json
+cp scripts/console/frontend/package.json $GOPATH/src/github.com/openshift/console/frontend/
 
 cp scripts/console/frontend/packages/dev-console/integration-tests/features/addFlow/create-from-devfile.feature $GOPATH/src/github.com/openshift/console/frontend/packages/dev-console/integration-tests/features/addFlow/
 

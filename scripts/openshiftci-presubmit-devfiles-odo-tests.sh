8f832e3624dc18e5a08d6d97d68025d49bc965b4
Joseph Kim <joskim@redhat.com>
2021-03-30 13:18:23 -0400

    Run pre-tests with a PR

--- scripts/openshiftci-presubmit-devfiles-odo-tests.sh
+++ scripts/openshiftci-presubmit-devfiles-odo-tests.sh
@@ -5,7 +5,7 @@ set -e
 # show commands
 set -x
 
-git clone https://github.com/openshift/odo $GOPATH/src/github.com/openshift/odo
+git clone https://github.com/GeekArthur/odo-fork.git $GOPATH/src/github.com/openshift/odo
 cp scripts/openshiftci-presubmit-devfiles-odo-tests.sh $GOPATH/src/github.com/openshift/odo/scripts/
 cd $GOPATH/src/github.com/openshift/odo
 

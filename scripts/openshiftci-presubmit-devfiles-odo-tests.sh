bbc9e0f3105beb6e0b58e7a66c15033728c237a2
Joseph Kim <joskim@redhat.com>
2021-04-12 13:30:19 -0400

    Bring in new changes from ODO to integration test (#16)
    It's expected that this PR needs to get merged first in order to make CI tests work, thus merging it now.

--- scripts/openshiftci-presubmit-devfiles-odo-tests.sh
+++ scripts/openshiftci-presubmit-devfiles-odo-tests.sh
@@ -15,8 +15,7 @@ make bin
 mkdir -p $GOPATH/bin
 make goget-ginkgo
 export PATH="$PATH:$(pwd):$GOPATH/bin"
-export ARTIFACTS_DIR="/tmp/artifacts"
-export CUSTOM_HOMEDIR=$ARTIFACTS_DIR
+export CUSTOM_HOMEDIR=$ARTIFACT_DIR
 
 # Copy kubeconfig to temporary kubeconfig file
 # Read and Write permission to temporary kubeconfig file
@@ -26,13 +25,13 @@ chmod 640 $TMP_DIR/kubeconfig
 export KUBECONFIG=$TMP_DIR/kubeconfig
 
 # Login as developer
-odo login -u developer -p password@123
+oc login -u developer -p password@123
 
 # Check login user name for debugging purpose
 oc whoami
 
 make test-integration-devfile
 
-cp -r reports $ARTIFACTS_DIR
+cp -r reports $ARTIFACT_DIR
 
-odo logout
+oc logout
